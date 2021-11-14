14a3e0e9a4368ff22ad92e91f574c4b59b16e341
Mihai Blebea <mihaiserban.blebea@gmail.com>
2021-11-14 19:47:21 +0000

    adds proxy secret

--- src/api.py
+++ src/api.py
@@ -1,13 +1,32 @@
 from flask import Flask, jsonify, request, abort
 import os
 import time
+from dotenv import dotenv_values
 
 from store import get_scrape_results
 
 app = Flask(__name__)
 
+config = dotenv_values(".env")
+
+def get_proxy_secret_env() -> str:
+	config = dotenv_values(".env")
+	if "RAPIDAPI_PROXY_SECRET" not in config:
+		return os.getenv("RAPIDAPI_PROXY_SECRET")
+
+	return config["RAPIDAPI_PROXY_SECRET"]
+
+
 @app.route("/scrape", methods=["GET"])
 def scrape():
+	proxy_secret = request.headers.get("X-RapidAPI-Proxy-Secret")
+	
+	if proxy_secret is None:
+		return abort(401)
+
+	if proxy_secret != get_proxy_secret_env():
+		return abort(401)
+
 	url = request.args.get("url")
 	if url is None:
 		return jsonify({
